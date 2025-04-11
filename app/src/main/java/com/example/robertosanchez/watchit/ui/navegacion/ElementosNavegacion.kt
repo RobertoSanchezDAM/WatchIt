552a70be2e27b1b0bcc4c59583750c2e5c6ed607
roberto <roberto@wiqrumedia.com>
2025-04-11 13:56:40 +0200

    Initial commit

---
+++ app/src/main/java/com/example/robertosanchez/watchit/ui/navegacion/ElementosNavegacion.kt
@@ -0,0 +1,18 @@
+package com.example.robertosanchez.watchit.ui.navegacion
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+object Inicio
+
+@Serializable
+object Login
+
+@Serializable
+object Usuario
+
+@Serializable
+object ContraseñaOlv
+
+@Serializable
+object Registro
---
+++ app/src/main/java/com/example/robertosanchez/watchit/ui/navegacion/Navegacion.kt
@@ -0,0 +1,57 @@
+package com.example.robertosanchez.watchit.ui.navegacion
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
+import com.example.robertosanchez.proyectoapi.data.AuthManager
+import com.example.robertosanchez.proyectoapi.ui.screens.contrasenaOlvScreen.ContrasenaOlvScreen
+import com.example.robertosanchez.proyectoapi.ui.screens.inicioScreen.InicioScreen
+import com.example.robertosanchez.proyectoapi.ui.screens.loginScreen.LoginScreen
+import com.example.robertosanchez.proyectoapi.ui.screens.registroScreen.RegistroScreen
+
+
+@Composable
+fun Navegacion(auth: AuthManager) {
+    val navController = rememberNavController()
+    val context = LocalContext.current
+
+    NavHost(navController = navController, startDestination = Inicio) {
+        composable<Inicio> {
+            InicioScreen({
+                    navController.navigate(Login)
+                }
+            )
+        }
+
+        composable<Login> {
+            LoginScreen(
+                auth,
+                { navController.navigate(Registro) },
+                {
+                    navController.navigate(Usuario) {
+                        popUpTo(Login) { inclusive = true }
+                    }
+                },
+                { navController.navigate(ContraseñaOlv) },
+                { navController.popBackStack() }
+            )
+        }
+
+        composable<Registro> {
+            RegistroScreen(
+                auth
+            ) { navController.popBackStack() }
+        }
+
+        composable<ContraseñaOlv> {
+            ContrasenaOlvScreen(
+                auth,
+                { navController.navigate(Login) }
+            )
+        }
+    }
+}
